6d664f09b45e68d749eeb780fa8026f3d8d9be24
Melanie Warrick <warrick@google.com>
2017-06-27 14:27:56 -0700

    Add license and contributing

--- hadoop_hive_practice.sql
+++ hadoop_hive_practice.sql
@@ -1,4 +1,18 @@
- // connect to use hive over hadoop
+-- Copyright 2016 Google Inc. All Rights Reserved.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+// connect to use hive over hadoop
  hadoop@domU-12-31-39-07-D214:~$ hive \
     -d SAMPLE=s3://elasticmapreduce/samples/hive-ads \
     -d DAY=2009-04-13 -d HOUR=08 \
